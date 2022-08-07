aac2f8071573208be5808cd5c0fb1bc3f097ced2
Erez Konforti <Erez.Konforti@Dell.com>
2022-08-07 12:21:17 +0300

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,26 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAGI20pwFF9seMovFRKPCv9E08c--kNGYY",
+    authDomain: "guessmyword-cae56.firebaseapp.com",
+    projectId: "guessmyword-cae56",
+    storageBucket: "guessmyword-cae56.appspot.com",
+    messagingSenderId: "45512826786",
+    appId: "1:45512826786:web:abadd2798c0af24870dcfb",
+    measurementId: "G-8RY76PLK19"
+  },
+  languages: [
+    {
+      value: 'hebrew',
+      display: 'עברית'
+     },
+     {
+      value: 'english',
+      display: 'English'
+     }
+    ],
+  defaultLanguage: {
+    value: 'hebrew',
+    display: 'עברית'
+   }
 };
